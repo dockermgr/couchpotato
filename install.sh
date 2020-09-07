d8372a4b7b350d5ebda9f46fad3c42b4c446dc0c
casjay <casjay@yahoo.com>
2020-09-07 04:14:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,53 @@
-mkdir -p /var/lib/docker/storage/couchpotato/transmission-daemon/{etc,watch,downloads} && chmod -Rf 777 /var/lib/docker/storage/couchpotato/transmission-daemon
+#!/usr/bin/env bash
 
-docker run -d --name=transmission-couchpotato \
+APPNAME="transmission"
+USER="${SUDO_USER:-${USER}}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# @Author      : Jason
+# @Contact     : casjaysdev@casjay.net
+# @File        : dockermgr
+# @Created     : Wed, Aug 05, 2020, 02:00 EST
+# @License     : WTFPL
+# @Copyright   : Copyright (c) CasjaysDev
+# @Description : docker script to manage containers
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Set functions
+
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/master/functions}"
+SCRIPTSFUNCTDIR="${SCRIPTSAPPFUNCTDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-applications.bash}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [ -f "$PWD/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$PWD/functions/$SCRIPTSFUNCTFILE"
+elif [ -f "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE"
+else
+  mkdir -p "/tmp/CasjaysDev/functions"
+  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/CasjaysDev/functions/$SCRIPTSFUNCTFILE" || exit 1
+  . "/tmp/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+[ "$1" = "--version" ] && get_app_info "$APPNAME"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+mkdir -p $APPDIR/$APPNAME/{etc,watch,downloads} && chmod -Rf 777 $APPDIR/$APPNAME
+
+docker run -d \
+--name=$APPNAME \
 --restart always \
--v /var/lib/docker/storage/couchpotato/transmission-daemon/etc:/config \
--v /var/lib/docker/storage/couchpotato/transmission-daemon/downloads:/downloads \
--v /var/lib/docker/storage/couchpotato/transmission-daemon/watch:/watch \
+-v  $APPDIR/$APPNAME/etc:/config \
+-v  $APPDIR/$APPNAME/downloads:/downloads \
+-v  $APPDIR/$APPNAME/watch:/watch \
 -v /mnt/torrents:/mnt/torrents \
 -p 9091:9091 \
 -p 51413:51413 \
--p 51413:51413/udp --restart=always  \
-registry.casjay.in/latest/transmission:latest
+-p 51413:51413/udp \
+linuxserver/transmission
