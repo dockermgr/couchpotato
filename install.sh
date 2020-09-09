9147783d8d8f3ac623fc79a92a1ebb68c7168fba
casjay <casjay@yahoo.com>
2020-09-09 16:13:27 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,51 +1,20 @@
 #!/usr/bin/env bash
 
 APPNAME="transmission"
-USER="${SUDO_USER:-${USER}}"
+DATADIR="/srv/docker/$APPNAME"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# @Author      : Jason
-# @Contact     : casjaysdev@casjay.net
-# @File        : dockermgr
-# @Created     : Wed, Aug 05, 2020, 02:00 EST
-# @License     : WTFPL
-# @Copyright   : Copyright (c) CasjaysDev
-# @Description : docker script to manage containers
-#
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+mkdir -p "$DATADIR"/{config,watch,downloads} && chmod -Rf 777 "$DATADIR"
 
-# Set functions
-
-SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/master/functions}"
-SCRIPTSFUNCTDIR="${SCRIPTSAPPFUNCTDIR:-/usr/local/share/CasjaysDev/scripts}"
-SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-applications.bash}"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-if [ -f "$PWD/functions/$SCRIPTSFUNCTFILE" ]; then
-  . "$PWD/functions/$SCRIPTSFUNCTFILE"
-elif [ -f "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE" ]; then
-  . "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE"
+if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
+docker pull linuxserver/transmission && docker restart $APPNAME
 else
-  mkdir -p "/tmp/CasjaysDev/functions"
-  curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/CasjaysDev/functions/$SCRIPTSFUNCTFILE" || exit 1
-  . "/tmp/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
-fi
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-[ "$1" = "--version" ] && get_app_info "$APPNAME"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-mkdir -p $APPDIR/$APPNAME/{etc,watch,downloads} && chmod -Rf 777 $APPDIR/$APPNAME
 
 docker run -d \
 --name=$APPNAME \
 --restart always \
--v  $APPDIR/$APPNAME/etc:/config \
--v  $APPDIR/$APPNAME/downloads:/downloads \
--v  $APPDIR/$APPNAME/watch:/watch \
+-v  $DATADIR/config:/config \
+-v  $DATADIR/downloads:/downloads \
+-v  $DATADIR/watch:/watch \
 -v /mnt/torrents:/mnt/torrents \
 -p 9091:9091 \
 -p 51413:51413 \
