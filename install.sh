af2a50998340f908b58a968e2c2e064d201cacf0
Jason <jason@casjay.net>
2021-01-29 23:28:05 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
 APPNAME="transmission"
-DATADIR="/srv/docker/$APPNAME"
 
 mkdir -p "$DATADIR"/{config,watch,downloads} && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker pull linuxserver/transmission && docker restart $APPNAME
+  docker pull linuxserver/transmission && docker restart "$APPNAME"
 else
 
-docker run -d \
---name=$APPNAME \
---restart always \
--v  $DATADIR/config:/config \
--v  $DATADIR/downloads:/downloads \
--v  $DATADIR/watch:/watch \
--v /mnt/torrents:/mnt/torrents \
--p 9091:9091 \
--p 51413:51413 \
--p 51413:51413/udp \
-linuxserver/transmission
+  docker run -d \
+    --name=$APPNAME \
+    --restart always \
+    -v "$DATADIR/config":/config \
+    -v "$DATADIR/downloads":/downloads \
+    -v "$DATADIR/watch":/watch \
+    -v /mnt/torrents:/mnt/torrents \
+    -p 9091:9091 \
+    -p 51413:51413 \
+    -p 51413:51413/udp \
+    linuxserver/transmission
 fi
