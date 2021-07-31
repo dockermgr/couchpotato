91dafefb2b147e2b268e6317de0f95a9e2ffa6ee
Jason <jason@casjay.net>
2021-07-31 18:10:47 -0400

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -59,6 +59,7 @@ if [ -f "$INSTDIR/docker-compose.yml" ]; then
   printf_blue "Installing containers using docker compose"
   sed -i "s|REPLACE_DATADIR|$DATADIR" "$INSTDIR/docker-compose.yml"
   if cd "$INSTDIR"; then
+    sudo docker rm "$APPNAME" -f &>/dev/null
     sudo docker-compose pull &>/dev/null
     sudo docker-compose up -d &>/dev/null
   fi
@@ -88,9 +89,8 @@ fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if docker ps -a | grep -qs "$APPNAME"; then
   printf_blue "Service is available at: http://$HOSTNAME:9091/"
-  printf_green "Successfully setup transmission"
 else
-  printf_return "Could not setup transmission"
+  false
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # End script
